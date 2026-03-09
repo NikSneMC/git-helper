f8c735f74f8dcb79120926abdd2381281d41a250
NikSne <commits@niksne.ru>
2026-03-09 16:44:05 +0300

    feat(clone): create a worktree for the default branch

--- src/commands/clone.rs
+++ src/commands/clone.rs
@@ -2,7 +2,9 @@ use std::{env, path::Path, process, time::Duration};
 
 use anyhow::Context as _;
 use clap::Parser;
-use git2::{Cred, FetchOptions, RemoteCallbacks, build::RepoBuilder};
+use git2::{
+    Cred, FetchOptions, RemoteCallbacks, Repository, WorktreeAddOptions, build::RepoBuilder,
+};
 use indicatif::{ProgressBar, ProgressStyle};
 
 use crate::{
@@ -55,33 +57,51 @@ impl Command for CloneOptions {
         }
 
         let current_dir = env::current_dir().context("while getting current directory")?;
-        let repo_path = config.base_dir.join(host).join(url).join(".git");
-        let repo_path_str = repo_path.to_string_lossy();
-        let repo_path_str = repo_path_str
-            .strip_suffix("/.git")
-            .context("while stripping suffix from repo path string")?;
+        let repo_path = config.base_dir.join(host).join(url);
+        let repo_path_str = repo_path.to_string_lossy().to_string();
 
         let spinner = ProgressBar::new_spinner()
             .with_message(format!("Cloning into {repo_path_str}"))
             .with_style(ProgressStyle::default_spinner().tick_chars("⣾⣽⣻⢿⡿⣟⣯⣷ "));
         spinner.enable_steady_tick(Duration::from_millis(100));
 
-        rb.clone(&git_url, &repo_path)
+        rb.clone(&git_url, &repo_path.join(".git"))
             .context("while cloning repository")?;
 
         env::set_current_dir(&repo_path).context("while changing the current directory")?;
         profile.apply().context("while applying profile")?;
+
+        let repo = Repository::open(&repo_path)
+            .context("while opening git repository in the current directory")?;
+        let origin = repo
+            .find_reference("refs/remotes/origin/HEAD")
+            .context("while finding origin reference")?
+            .resolve()
+            .context("while resolving origin")?;
+
+        let worktree_name = origin
+            .shorthand()
+            .context("while getting origin shorthand")?
+            .strip_prefix("origin/")
+            .context("while removing origin prefix")?;
+
+        let mut opts = WorktreeAddOptions::new();
+        opts.checkout_existing(true);
+        let worktree_path = repo_path.join(worktree_name);
+        repo.worktree(worktree_name, &worktree_path, Some(&opts))
+            .context("while creating a new git worktree")?;
+
+        // zoxide is not required so we don't care about command failure
+        let _ = process::Command::new("zoxide")
+            .arg("add")
+            .arg(&repo_path_str)
+            .output();
+
         env::set_current_dir(&current_dir).context("while changing the current directory")?;
 
         spinner.finish_with_message(format!(
             "Repository was cloned to `{repo_path_str}` successfully"
         ));
-
-        // zoxide is not required so we don't care about command failure
-        let _ = process::Command::new("zoxide")
-            .arg("add")
-            .arg(repo_path_str)
-            .output();
 
         Ok(())
     }
